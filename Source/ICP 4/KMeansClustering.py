b3af2e5cd62b728737c134354eb94c95c1124817
prudhvireddym <prudhvireddy.m@gmail.com>
2019-02-23 20:35:41 -0600

    ICP 5

--- Source/ICP 4/KMeansClustering.py
+++ Source/ICP 4/KMeansClustering.py
@@ -1,73 +1,59 @@
-# http://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html
-from sklearn.cluster import KMeans
-import numpy as numpy
-import pandas as pandas
-from matplotlib import pyplot as plot
+# Support Vector Machine  Classification Using Linear and RBF Kernel
 
-# Reading CSV File
-"""
-# 64-bit floating-point number - f8
-# 64-bit Integer - i8
-header=1 removes the 1st row
-"""
-data = pandas.read_csv('sample_stocks.csv', sep=',', header=1, dtype=[('int', 'i8'), ('float', 'f8')])
-csvData = data.values
+# Importing the libraries
 
-# Plotting initial array data
-plot.scatter(csvData[:, 0], csvData[:, 1])
-plot.show()
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+import pandas as pd
 
-# Creating K-means Cluster with 2 clusters
-kmeans = KMeans(n_clusters=2)
-kmeans.fit(csvData)
+# Importing the dataset
 
-# To See Centroid values the algorithm generated for the final clusters
-print(kmeans.cluster_centers_)
+dataset = pd.read_csv('D:\Github\CS5590-Python\Source\ICP 4\iris.csv')
 
-# To get the Labels
-print(kmeans.labels_)
 
-# Plotting the Data Points along with the labels.
-"""
-Here we are plotting the first column of the csvData array against the second column
-We are also passing kmeans.labels_ as value for the c parameter that corresponds to labels. 
-The cmap='rainbow' parameter is passed for choosing the color type for the different data points
-"""
-plot.scatter(csvData[:, 0], csvData[:, 1], c=kmeans.labels_, cmap='rainbow')
-plot.show()
+# looking at the first 5 values of the dataset
 
-"""Plotting the points along with the centroid coordinates of each cluster,
-to see how the centroid positions effects clustering.
-color='black' for the centroids"""
-plot.scatter(csvData[:, 0], csvData[:, 1], c=kmeans.labels_, cmap='rainbow')
-plot.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], color='black')
-plot.show()
+dataset.head()
 
-# With Increase in number of Clusters, we will get the datapoints clustering
-# together based on the number.
 
-"""
-Elbow Method - Technique to determine the K
-Location of a bend (knee) in the plot is generally considered as an indicator of the appropriate number of clusters.
+# Spliting the dataset in independent and dependent variables
 
-SSE --> Sum Of Squared Errors
+X = dataset.iloc[:, :4].values  # taking only  4 features for prediction
+Y = dataset['class'].values
 
-Plot a line graph of the SSE for each value of k. 
-If the line graph looks like an arm - a red circle in below line graph (like angle), 
-the "elbow" on the arm is the value of optimal k (number of cluster). 
-Here, we want to minimize SSE. SSE tends to decrease toward 0 as we increase k 
-(and SSE is 0 when k is equal to the number of data points in the dataset, 
-because then each data point is its own cluster, and there is no error between it and the center of its cluster).
 
-So the goal is to choose a small value of k that still has a low SSE, and the elbow usually represents 
-where we start to have diminishing returns by increasing k.
-"""
-distortions = {}
-# Selecting k in range of 1 to 10
-for k in range(1, 10):
-    kmeans = KMeans(n_clusters=k).fit(csvData)
-    # Inertia: Sum of distances of samples to their closest cluster center
-    distortions[k] = kmeans.inertia_
+# Splitting the dataset into the Training set and Test set
 
-plot.plot(list(distortions.keys()), list(distortions.values()))
-plot.show()
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.20)
+
+
+# Fitting SVC Classification to the Training set with linear kernel
+
+svc_linear = SVC(kernel='linear', C=1, gamma=0.1).fit(X_train, y_train)
+
+
+# Accuracy of SVM Linear kernel on Training set
+
+print('\n\nAccuracy of the SVM Linear Kernel Classification on training part is: ', svc_linear.score(X_train, y_train))
+
+
+# Accuracy of SVM Linear Kernel on Test Set
+
+print('\n\nAccuracy of the SVM Linear Kernel Classification on testing part is: ', svc_linear.score(X_test, y_test))
+
+# Fitting SVC Classification to the Training set with rbf  kernel
+
+
+svc_rbf = SVC(kernel='rbf', C=1, gamma=0.1).fit(X_train, y_train)
+
+
+# Accuracy of SVM RBF kernel on Training set
+
+print('\n\nAccuracy of the SVM RBF Kernel Classification on training part is: ', svc_rbf.score(X_train, y_train))
+
+
+# Accuracy of SVM RBF Kernel on Test Set
+
+print('\n\nAccuracy of the SVM  RBF Kernel Classification on testing part is: ', svc_rbf.score(X_test, y_test))
--- Source/ICP 4/NaiveBayes.py
+++ Source/ICP 4/NaiveBayes.py
@@ -1,43 +1,75 @@
-# Required Imports
-from sklearn import datasets
-from sklearn.naive_bayes import GaussianNB
-from matplotlib import pyplot as plot
+# Naive Bayes Classification
+
+# Importing the libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+import pandas as pd
 
-# Loading Iris datasets
-iris = datasets.load_iris()
+# sklearn.naive_bayes.GaussianNB(priors=None, var_smoothing=1e-09)
 
-# Fitting Naive Bayes model to the data
-model = GaussianNB()
-model.fit(iris.data, iris.target)
-print(model)
+# Importing the dataset
 
-# Making Prediction based on X, Y
-expected = iris.target
-predicted = model.predict(iris.data)
-print(expected,":", predicted)
-# Matlib Plot
-plot.plot(expected, predicted)
-plot.show()
+dataset = pd.read_csv('D:\Github\CS5590-Python\Source\ICP 4\iris.csv')
 
-# Cross Validation
 
-print(metrics.classification_report(expected, predicted))
-# confusion_matrix - To evaluate Accuracy of classification
-print(metrics.confusion_matrix(expected, predicted))
+# looking at the first 5 values of the dataset
 
-# splitting data and target into training and testing sets
-"""http://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html"""
-# If int, random_state is the seed used by the random number generator
-# If float, test_size represent the proportion of the dataset to include in the test split [Default: 0.25]
-X_train, X_test, Y_train, Y_test = train_test_split(iris.data, iris.target, test_size=0.4, random_state=1)
+dataset.head()
 
-# Training the Model on Training Set
-model.fit(X_train, Y_train)
 
-# Training the Model on Testing Set
-Y_predicted = model.predict(X_test)
+# Spliting the dataset in independent and dependent variables
 
-# Evaluating the Model based on Testing Part
-print("Gaussian Model Accuracy is ", metrics.accuracy_score(Y_test, Y_predicted) * 100)
+X = dataset.iloc[:, :4].values
+Y = dataset['class'].values
+
+
+# Splitting the dataset into the Training set and Test set
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.20)
+
+
+# Fitting Naive Bayes Classification to the Training set with linear kernel
+
+nvclassifier = GaussianNB()
+nvclassifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+
+y_pred = nvclassifier.predict(X_test)
+
+# lets see the actual and predicted value side by side
+
+y_compare = np.vstack((y_test, y_pred)).T
+
+# actual value on the left side and predicted value on the right hand side
+# printing the top 5 values
+
+print(y_compare[:5, :])
+
+
+# Accuracy of NavesBayes on Training Sets
+print('Accuracy of Naive Bayes classifier on training set: {:.2f}'
+     .format(nvclassifier.score(X_train, y_train)))
+
+# Accuracy of NavesBayes on Testing Sets
+print('Accuracy of Naive Bayes classifier on test set: {:.2f}'
+     .format(nvclassifier.score(X_test, y_test)))
+
+
+# print the accuracy score of predicted and actual values on test set
+
+print('\n\nAccuracy of the Naive Bayes Classification on Test Data Prediction is : ', metrics.accuracy_score(y_test, y_pred))
+
+# Plot the Test Prediction , Test Value on Graph
+
+plt.scatter(y_test, y_pred)
+
+plt.xlabel('True Values')
+
+plt.ylabel('Predictions')
+
+plt.show()
