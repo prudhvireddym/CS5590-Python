ff4763bf588455197b72698fa9ea7263a73e40ac
ugandhar007 <singani.pegasian@gmail.com>
2019-03-25 20:57:14 -0500

    ICP 8

---
+++ Source/Deep Learning/ICP 8/dl2.py
@@ -0,0 +1,55 @@
+import pandas as pd
+from keras import Sequential
+from keras.layers import Dense
+from sklearn import datasets
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+import warnings
+warnings.simplefilter("ignore")
+
+# Load the train DataFrames using pandas
+dataset = pd.read_csv('C:/Users/laksh/PycharmProjects/DeepLearning_Lesson1/Breas Cancer.csv')
+
+
+
+#print("Train_Set description")
+#print(train.describe())
+#print("\n")
+
+
+
+# Fill missing or null values with mean column values in the train set
+import numpy as np
+
+
+
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder
+from sklearn import preprocessing
+labelEncoder = preprocessing.LabelEncoder()
+labelEncoder.fit(dataset['diagnosis'])
+dataset['diagnosis'] = labelEncoder.transform(dataset['diagnosis'])
+dataset.info()
+print(dataset.head())
+
+dataset=dataset.values
+
+
+X_train, X_test, Y_train, Y_test = train_test_split(dataset[:,2:32], dataset[:,1],
+                                                    test_size=0.25, random_state=87)
+np.random.seed(155)
+my_first_nn = Sequential() # create model
+my_first_nn.add(Dense(10, input_dim=30, activation='relu'))# hidden layer
+my_first_nn.add(Dense(12, activation='softplus'))
+#my_first_nn.add(Dense(156, activation='sigmoid')) #added one laye
+#my_first_nn.add(Dense(7, activation='sigmoid')) #added another layer
+my_first_nn.add(Dense(1, activation='sigmoid')) # output layer
+my_first_nn.compile(loss='binary_crossentropy', optimizer='adam')
+my_first_nn_fitted = my_first_nn.fit(X_train, Y_train, epochs=100, verbose=0,
+                                     initial_epoch=0)
+print(my_first_nn.summary())
+print(my_first_nn.evaluate(X_test, Y_test, verbose=0))
+from sklearn.metrics import accuracy_score
+#score = accuracy_score(dataset.target, pr)
+
